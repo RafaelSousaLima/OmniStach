c99834c5efc208ece4060ac4425889c1341370c8
RafaelSousaLima <rafaelsousalima@live.com>
2020-01-15 16:12:18 -0300

    Update index.js

--- OmniStack-Backend/src/index.js
+++ OmniStack-Backend/src/index.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://rafaellima:963*19283#Sousa@cluster0-0xw0g.mongodb.net/week10?retryWrites=true&w=majority'), {
+mongoose.connect('mongodb+srv://rafaellima:123456789@cluster0-0xw0g.mongodb.net/week10?retryWrites=true&w=majority'), {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 };
