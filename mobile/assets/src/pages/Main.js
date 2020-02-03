ab3be87445db25379b7b7e41a1b8c0c4ab4af692
Rafael Lima <rafaelsousalima@live.com>
2020-02-03 00:30:34 -0300

    Parte mobile

---
+++ mobile/assets/src/pages/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function Main() {
+    return <view></view>
+}
+
+export default Main;
---
+++ mobile/assets/src/pages/Profile.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function Profile() {
+    return <view></view>
+}
+
+export default Profile;
