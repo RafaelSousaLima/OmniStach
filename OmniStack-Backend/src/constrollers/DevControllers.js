873dc02db5061a5213ce2a4e2046d98f302d0469
Rafael Lima <rafaelsousalima@live.com>
2020-02-08 20:03:10 -0300

    Finalizado o crud do react js

--- OmniStack-Backend/src/constrollers/DevControllers.js
+++ OmniStack-Backend/src/constrollers/DevControllers.js
@@ -12,7 +12,6 @@ module.exports =  {
     async store(request, response) {
         const { github_username, techs, latitude, longitude } = request.body;
 
-        console.log(github_username);
         let dev = await Dev.findOne({ github_username });
         if (!dev) {
             const apiResponse = await axios.get(`http://api.github.com/users/${github_username}`);
@@ -34,8 +33,47 @@ module.exports =  {
                 techs: techsArray,
                 location
             });
+            return response.json(`Usuário ${github_username} criado com sucesso.`);
         }
-        return response.json(dev);
+        return response.json(`Usuário ${github_username} já existe dentro da aplicação.`);
     },
+
+    async update(request, response) {
+        const { github_username, techs, latitude, longitude } = request.body;
+
+        const techsArray = parseStringAsArray(techs);
+        
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+        };
+
+        let dev = await Dev.findOne({github_username});
+
+        if (dev) {
+           const devAtualizado = await Dev.updateOne(
+               { 
+                   'github_username' : github_username
+                }, { 
+                    'techs': techsArray, 'location': location 
+                }
+            );
+            return response.json({
+                'message': `Usuário ${github_username} atualizado com sucesso.`,
+                'dev': devAtualizado
+            });
+        } else {
+            return response.json(`Usuário ${github_username} não foi encontrado.`);
+        }
+    },
+
+    async delete(request, response) {
+        const { github_username } = request.query;
+        let devEncontrado = await Dev.findOneAndRemove({ github_username : github_username });
+        if (devEncontrado) {
+            return response.json(`Usuário ${github_username} deletado com sucesso.`);
+        }
+        return response.json(`Usuário ${github_username} não foi encontrado.`);
+    }
 
 };
--- OmniStack-Backend/src/routes.js
+++ OmniStack-Backend/src/routes.js
@@ -5,7 +5,9 @@ const SeachController = require('./constrollers/SeachControllers')
 const routes = Router();
 
 routes.get('/devs', DevController.index);
+routes.put('/devs', DevController.update);
 routes.post('/devs', DevController.store);
+routes.delete('/devs', DevController.delete);
 
 routes.get('/search', SeachController.index);
 
